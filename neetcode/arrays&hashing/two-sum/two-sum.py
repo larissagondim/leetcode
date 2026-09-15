14d231420054b29bfa03f15e97df9b07817ae624
larissagondim <gondimvilasboaslarissa@gmail.com>
2026-09-14 21:17:58 -0300

    feat: neetcode twosum

--- neetcode/arrays&hashing/two-sum/two-sum.py
+++ neetcode/arrays&hashing/two-sum/two-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = {nums[i]: i for i in range(len(nums))}  
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in dict and dict[complement] != i:
-                return [dict[complement], i]
+            comp = target - nums[i]
+            if comp in dict and dict[comp] != i:
+                return [i, dict[comp]]
+        
